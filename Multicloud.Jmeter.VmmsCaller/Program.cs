ff17f7c968f5ce9a2c813b131316449250bbde40
marcoslobo <marcos.lobo.nit@gmail.com>
2022-03-30 15:46:04 -0300

    initial

---
+++ Multicloud.Jmeter.VmmsCaller/Program.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.Azure.Management.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent;
+using Microsoft.Azure.Management.ResourceManager.Fluent.Core;
+using System.Diagnostics;
+
+
+
+var credentials = SdkContext.AzureCredentialsFactory.FromFile("my.azureauth");
+
+var azure = Azure
+    .Configure()
+    .WithLogLevel(HttpLoggingDelegatingHandler.Level.Basic)
+    .Authenticate(credentials).WithDefaultSubscription();
+
+var vmScaleSet = azure.VirtualMachineScaleSets.GetByResourceGroup("jmt-resources", "jmt-vmss");
+
+var networkInterfaces = vmScaleSet.ListNetworkInterfaces();
+
+var listaIps = new List<string>();
+
+foreach (var vmNetworkInterface in networkInterfaces)
+{
+    listaIps.Add(vmNetworkInterface.PrimaryPrivateIP);
+}
+
+var ipsNormalized = string.Join(",", listaIps);
+
+var output = $"./../../apache-jmeter-3.3/bin/jmeter -n -t ./../script.jmx -R {ipsNormalized} -l results.jtl -Jserver.rmi.ssl.disable=true".Bash();
+Console.WriteLine(output);
+
+public static class ShellHelper
+{
+    public static string Bash(this string cmd)
+    {
+        var escapedArgs = cmd.Replace("\"", "\\\"");
+
+        var process = new Process()
+        {
+            StartInfo = new ProcessStartInfo
+            {
+                FileName = "/bin/bash",
+                Arguments = $"-c \"{escapedArgs}\"",
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true,
+            }
+        };
+        process.Start();
+        string result = process.StandardOutput.ReadToEnd();
+        process.WaitForExit();
+
+        return result;
+    }
+}
