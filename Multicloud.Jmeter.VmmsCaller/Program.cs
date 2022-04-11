13e790595e6b3b7283f6093e50550412d0b01aea
Marcos lobo <marcos.lobo.nit@gmail.com>
2022-04-11 17:35:48 -0300

    ajustes

--- Multicloud.Jmeter.VmmsCaller/Program.cs
+++ Multicloud.Jmeter.VmmsCaller/Program.cs
@@ -32,7 +32,7 @@ var timestamp = DateTimeOffset.UtcNow.Ticks;
 //Console.WriteLine(folderr);
 
 //Executar
-var output = $"sudo ./../../../apache-jmeter-5.4.3/bin/jmeter -n -t ./../../script.jmx -R {ipsNormalized} -l results_{timestamp}.jtl -Jserver.rmi.ssl.disable=true -o ./home/ubuntu/{timestamp}/".Bash();
+var output = $"sudo ./../../../apache-jmeter-5.4.3/bin/jmeter -n -t ./../../script.jmx -R {ipsNormalized} -l results_{timestamp}.jtl -Jserver.rmi.ssl.disable=true -o /{timestamp}".Bash();
 Console.WriteLine(output);
 
 public static class ShellHelper
